e274d8e87e4ccd3d8b5892909fd5f4236d94a5c6
yeonnex <yeonnex@gmail.com>
2024-04-10 12:31:10 +0900

    feat: @JsonTest 를 사용한 JSON 직렬화 테스트

---
+++ src/test/java/com/yeonnex/catalogservice/web/BookJsonTests.java
@@ -0,0 +1,45 @@
+package com.yeonnex.catalogservice.web;
+
+import com.yeonnex.catalogservice.domain.Book;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest // JSON 직렬화에 중점을 둔 테스트 클래스임을 나타낸다
+public class BookJsonTests {
+    @Autowired
+    private JacksonTester<Book> json; // JSON 직렬화 및 역직렬화를 확인하기 위한 유틸리티 클래스
+
+    @Test
+    @DisplayName("직렬화 테스트")
+    void testSerialize() throws IOException {
+        var book = new Book("1234567890", "title", "author", 5.55);
+        var jsonContent = json.write(book);
+        assertThat(jsonContent).extractingJsonPathStringValue("@.isbn").isEqualTo(book.isbn());
+        assertThat(jsonContent).extractingJsonPathStringValue("@.title").isEqualTo(book.title());
+        assertThat(jsonContent).extractingJsonPathStringValue("@.author").isEqualTo(book.author());
+        assertThat(jsonContent).extractingJsonPathNumberValue("@.price").isEqualTo(book.price());
+    }
+
+    @Test
+    @DisplayName("역직렬화 테스트")
+    void testDeserialize() throws IOException {
+        var content = """
+                {
+                    "isbn": "1234567890",
+                    "title": "my title",
+                    "author": "my author",
+                    "price": 9.99
+                }
+                """;
+        assertThat(json.parse(content))
+                .usingRecursiveComparison()
+                .isEqualTo(new Book("1234567890", "my title", "my author", 9.99));
+    }
+}
